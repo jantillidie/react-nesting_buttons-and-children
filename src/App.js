1d1ccdc08cd874e976da8a58ed6db892aeda05e8
Jan Maaß <git@antipost.de>
2023-11-14 10:29:50 +0100

    Add solution

--- src/App.js
+++ src/App.js
@@ -4,18 +4,18 @@ import "./styles.css";
 export default function App() {
   return (
     <main>
-      <Button />
-      <Button />
-      <Button />
-      <Button />
+      <Button>Click me!</Button>
+      <Button>Do not click me!</Button>
+      <Button>Click another button</Button>
+      <Button>click</Button>
     </main>
   );
 }
 
-function Button() {
+function Button({ children }) {
   return (
     <button className="button" type="button">
-      Click me!
+      {children}
     </button>
   );
 }
